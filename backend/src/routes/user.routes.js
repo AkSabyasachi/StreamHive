d07dacb234961bc7d33eeb3f85838501cc014c2d
AK Sabyasachi <aksabyasachi730@gmail.com>
2025-06-30 20:48:17 +0530

    Added new routes for user controller

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,10 +1,21 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import {
+  changePassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  updateAvatar,
+  updateCoverImage,
+  updateUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-
-const router = Router()
+const router = Router();
 
 router.route("/register").post(
   //* use multer middleware to handle file uploads
@@ -12,23 +23,38 @@ router.route("/register").post(
   upload.fields([
     {
       name: "avatar",
-      maxCount:1,
+      maxCount: 1,
     },
     {
       name: "coverimage",
-      maxCount:1,
+      maxCount: 1,
     },
   ]),
-  
+
   //* this is the controller function that handles the registration logic so middleware logic is written just before the controller function
   registerUser
-)
+);
 
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
 //secured routes
-router.route("/logout").post( verifyJWT , logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changePassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-user").patch(verifyJWT, updateUser);
 
+//* Avatar and Cover Image Update
+router
+  .route("/update-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateAvatar);
+router
+  .route("/update-cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
+
+//* req.params is used to get the username from the URL
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router;
